46aec417f04a3139c12fb164338dc333f8be46ea
dimabytes <dimabytes@gmail.com>
2020-04-18 16:08:01 +0300

    check2

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,8 +6,8 @@ import {HomePage, ProductPage} from '../pages'
 const App = ({shopService}) => {
   return (
     <Switch>
-      <Route path="/leave-me/" component={HomePage} exact/>
-      <Route path="/leave-me/product/:id?" component={ProductPage}/>
+      <Route path="/" component={HomePage} exact/>
+      <Route path="/product/:id?" component={ProductPage}/>
     </Switch>
   )
 }
